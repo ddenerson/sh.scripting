d2a1686ffe3298615599eb5ee707c8356c0b9f92
Denerson Silva <ddenerson@gmail.com>
2020-09-14 22:57:39 -0300

    Projeto criado

---
+++ 01soma_valores.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+###################################################
+#                                                 #
+# soma_valores.sh -Soma de dois valores           #
+#                                                 #
+# Autor: Denerson Silva (ddenerson@gmail.com)     #
+# Data : 10/09/2020                               #
+#                                                 #
+# Descrição: Crie um script que após executado    #
+#            solicite dois valores como entrada   #
+#            Esses valores devem ser somados e    #
+#            o resultado exibido p/ o usuario     #
+#                                                 #
+# Exemplo de uso: ./soma_valores.sh               #
+#                                                 #
+##################################################
+
+echo "========= Script soma de dois valores ============="
+echo ""
+read -p "1° Valor:" VALOR1
+echo ""
+read -p "2° Valor:" VALOR2
+echo ""
+echo A SOMA DE $VALOR1 COM $VALOR2 E: $(($VALOR1+$VALOR2))
+echo ""
+echo "Fim do Script"
